d626a60e2d6cd45cdc8f3c6ff88206d665fee384
Ethan Chen <intervigil@gmail.com>
2017-09-17 13:14:00 +0800

    twolip: Add basic USB HAL that reports no status change
    Change-Id: I6d130d324753c03ac514c4500c5764bfa46941dc
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- device.mk
+++ device.mk
@@ -410,6 +410,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.xiaomi_sdm660
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service	
