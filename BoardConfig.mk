7c9f78247c1a5a9fa5f8ab942708dab4d8361892
Anand Shekhawat <anandsingh215@yahoo.com>
2022-01-29 08:47:15 +0000

    tulip: remove sepolicy-legacy-um include

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,7 +148,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
