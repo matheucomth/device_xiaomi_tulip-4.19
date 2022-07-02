61b030b9fda371dbdb46aacf586e13793e83a3fe
Nischay Sanjay <nischay.sanjay23@gmail.com>
2022-07-02 19:56:22 +0530

    tulip: Upgrade android.hardware.radio.config to 1.2 and downgrade android.hardware.radio to 1.4

--- device.mk
+++ device.mk
@@ -351,8 +351,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.radio.config@1.1 \
-    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio@1.4 \
     android.hardware.secure_element@1.2 \
     android.hardware.secure_element@1.2.vendor \
     libavservices_minijail.vendor \
